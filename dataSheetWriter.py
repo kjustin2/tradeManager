cb9e6f33e3ab0f4028e591e09b6fe53fb9a77c9c
Justin Kramer <jpk91@pitt.edu>
2021-11-16 19:15:51 -0500

    Add new column to data result sheet with latest call number

--- dataSheetWriter.py
+++ dataSheetWriter.py
@@ -58,15 +58,18 @@ def writeFormulaInformation(transaction, dataSheet, currentRow, writeToLog):
                        14, currentRow, Helper.getValueFormula(transaction.symbol))
     Helper.writeToFile(writeToLog, 'currentAsk', dataSheet,
                        15, currentRow, Helper.getAskFormula(transaction))
+    Helper.writeToFile(writeToLog, 'currentCall', dataSheet,
+                       16, currentRow, Helper.getCallFormula(transaction))
+
 
     Helper.writeToFile(writeToLog, 'NetPrem', dataSheet,
-                       16, currentRow, """=IF([TransType]=\"LS\", [OpnPrem]+[ClsPrem],
+                       17, currentRow, """=IF([TransType]=\"LS\", [OpnPrem]+[ClsPrem],
                                             IF([TransType]=\"AS\", [OpnPrem]+[ClsPrem],
                                                [OpnPrem]-[ClsPrem]))""")
     Helper.writeToFile(writeToLog, 'TotPrem', dataSheet,
-                       17, currentRow, """=IF([SYM]="","",SUMIFS([NetPrem],[Trade'#],[[Trade'#]],[Leg],"<="&[Leg]))""", moneyFormat=True)
+                       18, currentRow, """=IF([SYM]="","",SUMIFS([NetPrem],[Trade'#],[[Trade'#]],[Leg],"<="&[Leg]))""", moneyFormat=True)
     Helper.writeToFile(writeToLog, 'Days', dataSheet,
-                       18, currentRow, """=IF(INDIRECT("RC[-14]", FALSE)="DEP",0,
+                       19, currentRow, """=IF(INDIRECT("RC[-14]", FALSE)="DEP",0,
 IF(INDIRECT("RC[-14]", FALSE)="DIV",0,
 IF(INDIRECT("RC[-14]", FALSE)="LS",TODAY() - INDIRECT("RC[-13]", FALSE),
 IF(INDIRECT("RC[-14]", FALSE)="AS",TODAY() - INDIRECT("RC[-13]", FALSE),
@@ -74,61 +77,61 @@ IF(INDIRECT("RC[-12]", FALSE)>0,
 IF(INDIRECT("RC[-12]", FALSE) - INDIRECT("RC[-13]", FALSE)>0,INDIRECT("RC[-12]", FALSE) - INDIRECT("RC[-13]", FALSE),1),
 INDIRECT("RC[-11]", FALSE) - INDIRECT("RC[-13]", FALSE))))))""")
     Helper.writeToFile(writeToLog, 'Cap', dataSheet,
-                       19, currentRow, """=IF([TransType]="SP",100*[Strike]*[['#Contracts]],
+                       20, currentRow, """=IF([TransType]="SP",100*[Strike]*[['#Contracts]],
 IF([TransType]="LS",[Strike]*[['#Contracts]],
 IF([TransType]="BP",100*([Strike]-[Strike2])*[['#Contracts]],
 IF([TransType]="BC",100*([Strike2]-[Strike])*[['#Contracts]],
 IF([TransType]="NC",100*[Strike]*[['#Contracts]],
 IF([TransType]="AS",100*[Strike]*[['#Contracts]],0))))))""")
     Helper.writeToFile(writeToLog, 'OpenCap', dataSheet,
-                       20, currentRow, """=IF([CloseDate]>0,"",[Cap])""")
+                       21, currentRow, """=IF([CloseDate]>0,"",[Cap])""")
     Helper.writeToFile(writeToLog, 'BEcap', dataSheet,
-                       21, currentRow, """=IF([CloseDate]>0,"",
+                       22, currentRow, """=IF([CloseDate]>0,"",
 IF([TransType]="BC","",
 IF([TransType]="BP",100*[Strike]*[['#Contracts]],
 [Cap])))""")
     Helper.writeToFile(writeToLog, 'CapDays', dataSheet,
-                       22, currentRow, """=IF([SYM]="","",[Cap]*[Days])""")
+                       23, currentRow, """=IF([SYM]="","",[Cap]*[Days])""")
     Helper.writeToFile(writeToLog, 'TotCapDays', dataSheet,
-                       23, currentRow, """=IF([SYM]="","",SUMIFS([CapDays],[Trade'#],[[Trade'#]],[Leg],"<="&[Leg]))""")
+                       24, currentRow, """=IF([SYM]="","",SUMIFS([CapDays],[Trade'#],[[Trade'#]],[Leg],"<="&[Leg]))""")
     Helper.writeToFile(writeToLog, 'AROI', dataSheet,
-                       24, currentRow, """=IF([TotCapDays],365*[TotPrem]/[TotCapDays],"")""", percentFormat=True, color='FFFF00')
+                       25, currentRow, """=IF([TotCapDays],365*[TotPrem]/[TotCapDays],"")""", percentFormat=True, color='FFFF00')
     Helper.writeToFile(writeToLog, 'BreakEven', dataSheet,
-                       25, currentRow, """=IF([SYM]="","",
+                       26, currentRow, """=IF([SYM]="","",
 IF([TransType]="LS",[Strike]-[TotPrem]/[['#Contracts]],
 IF([['#Contracts]],[Strike]-[TotPrem]/[['#Contracts]]/100,"")))""", twoDecimalsFormat=True)
     Helper.writeToFile(writeToLog, 'ActShares', dataSheet,
-                       26, currentRow, """=IF([CloseDate]>0,"",
+                       27, currentRow, """=IF([CloseDate]>0,"",
 IF([TransType]="LS",['#Contracts],
 IF([TransType]="AS",100*[['#Contracts]],
 IF([TransType]="SP",100*[['#Contracts]],
 IF([TransType]="BP",100*[['#Contracts]],"")))))""")
     Helper.writeToFile(writeToLog, 'ActDate', dataSheet,
-                       27, currentRow, """=IF([CloseDate]>0,[CloseDate],
+                       28, currentRow, """=IF([CloseDate]>0,[CloseDate],
 IF([ExpDate]>0,[ExpDate],
 TODAY()))""", dateFormat=True)
     Helper.writeToFile(writeToLog, 'Inception', dataSheet,
-                       28, currentRow, """=IF(PERFORMANCE!D8>0,
+                       29, currentRow, """=IF(PERFORMANCE!D8>0,
 IF(PERFORMANCE!D8<[ActDate],"",[NetPrem]),
 IF(TODAY()<[ActDate],"",[NetPrem]))""")
     Helper.writeToFile(writeToLog, '1YR', dataSheet,
-                       29, currentRow, """=IF(PERFORMANCE!D8>0,
+                       30, currentRow, """=IF(PERFORMANCE!D8>0,
 IF(PERFORMANCE!D8<[ActDate],"",
 IF(PERFORMANCE!D8 - [ActDate]<366,[NetPrem],"")),
 IF(TODAY()<[ActDate],"",
 IF(TODAY() - [ActDate]<366,[NetPrem],"")))""")
     Helper.writeToFile(writeToLog, 'YTD', dataSheet,
-                       30, currentRow, """=IF(PERFORMANCE!D8>0,
+                       31, currentRow, """=IF(PERFORMANCE!D8>0,
 IF(PERFORMANCE!D8<[ActDate],"",
 IF(YEAR(PERFORMANCE!D8)=YEAR([ActDate]),[NetPrem],"")),
 IF(TODAY()<[ActDate],"",
 IF(YEAR(TODAY())=YEAR([ActDate]),[NetPrem],"")))""")
     Helper.writeToFile(writeToLog, 'UnBooked', dataSheet,
-                       31, currentRow, """=IF(PERFORMANCE!D8>0,
+                       32, currentRow, """=IF(PERFORMANCE!D8>0,
 IF(PERFORMANCE!D8<[ActDate],[NetPrem],""),
 IF(TODAY()<[ActDate],[NetPrem],""))""")
     Helper.writeToFile(writeToLog, 'TotPrem', dataSheet,
-                       33, currentRow, """=IF(Table1[SYM]="","",SUMIFS(Table1[NetPrem],Table1[Trade'#],Table1[[Trade'#]],Table1[Leg],"<="&Table1[Leg]))""")
+                       34, currentRow, """=IF(Table1[SYM]="","",SUMIFS(Table1[NetPrem],Table1[Trade'#],Table1[[Trade'#]],Table1[Leg],"<="&Table1[Leg]))""")
 
 
 def addTable(dataSheet, startingRow, endRow):
--- helperMethods.py
+++ helperMethods.py
@@ -89,6 +89,22 @@ def getAskFormula(newTransaction):
     return base + symbolDate + '")'
 
 
+def getCallFormula(newTransaction):
+    base = '=RTD("tos.rtd",,"LAST",".'
+    strike = str(newTransaction.strike)
+    strike = strike.replace('.0', '')
+    day = str(newTransaction.day)
+    if len(day) == 1:
+        day = '0' + day
+    month = str(getMonthNumber(newTransaction.month))
+    if len(month) == 1:
+        month = '0' + month
+    year = str(newTransaction.year)[2:]
+    symbolDate = newTransaction.symbol + year + \
+        month + day + 'C' + strike
+    return base + symbolDate + '")'
+
+
 def printWithNewLines(message):
     print('\n' + message + '\n')
 
